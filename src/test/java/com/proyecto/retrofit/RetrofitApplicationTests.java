199915acfb243be42ecc2411b9340ffbbda2b372
NuevosProyecto <proyectonuevos.2019@gmail.com>
2020-06-30 13:02:13 -0500

    feat: implementa estructura del microservicio

---
+++ src/test/java/com/proyecto/retrofit/RetrofitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.retrofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
